dfad123152c9f00ea4a8d37b72e825fc0b01b255
sagarsaini3993 <sagarsaini3993@gmail.com>
2021-04-14 00:11:59 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {createComponent} from "./components/CreateComponent";
+import {IndexComponent} from "./components/IndexComponent";
+import {EditComponents} from "./components/EditComponents";
+import {NameList} from "./components/NameList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      ItemArray : [{
+        name:'Rahul',phone:'1212',city:'Toronto',email:'rahul@gmail.com', image:'1.jpg'
+      }]
+    }
+    render(){
+      return(
+        <Router>
+          <div className = "container">
+            <nav className = "navbar navbar-expand"
+          </div>
+        </Router>
+      )
+    }
+  }
+  
 }
 
-export default App;
+App.defaultProps={
+  headerProp:"Hello header"
+}
+
+    export default App;
